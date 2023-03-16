397d9f057459bb3a8b9567544fdeced986c654b7
Max Santiphab <104512737+MaxSoudPouk@users.noreply.github.com>
2023-03-16 23:45:46 +0700

    update

--- src/main/java/com/example/VintageShopAPI/CheckOut/CheckoutController.java
+++ src/main/java/com/example/VintageShopAPI/CheckOut/CheckoutController.java
@@ -36,7 +36,7 @@ public class CheckoutController {
     )  throws NoSuchAlgorithmException, IOException, JSONException, JSONPointerException {
         System.out.println("order =====" + orders);
 
-        Map<String, String> person1 = new HashMap<>();
+
         Map<String, Object> response = new HashMap<>();
         List<Map<String, String>> resultMsg = new ArrayList<>();
 
@@ -44,7 +44,7 @@ public class CheckoutController {
         JSONArray order = jObj.getJSONArray("order");
         String customer_id = jObj.getString("customer_id");
         String order_status = jObj.getString("order_status");
-        String total_price = jObj.getString("total_price");
+        double total_price = jObj.getDouble("total_price");
         String order_id = jObj.getString("order_id");
         String customer_name = jObj.getString("customer_name");
         String transaction_id = jObj.getString("transaction_id");
@@ -77,27 +77,39 @@ public class CheckoutController {
             connection1 = dbConnectionPool.getConnection();
 
         String sql = "insert into order_tb (order_id, customer_id, total_price, order_date, order_status, transaction_id, customer_name) values (?, ?, ?, ?, ?, ?, ?)";
-        String sql1 = "SELECT product_amount FROM products_attribute WHERE product_id = ? AND price = ?";
+        String sql1 = "SELECT pa.product_amount,pt.product_status " +
+                "FROM products_attribute pa " +
+                "JOIN products_tb pt ON pa.product_id = pt.product_id " +
+                "WHERE pa.product_id = ? AND pa.price = ? ";
         String sql2 = "insert into products_attribute (product_amount) values (?)";
-            String updateProductAmount = "UPDATE products_attribute SET product_amount = ? WHERE product_id = ?";
+        String updateProductAmount = "UPDATE products_attribute SET product_amount = ? WHERE product_id = ?";
         String updateProductStatus = "UPDATE products_tb SET product_status = ? WHERE product_id = ?";
 
 
             for (int i = 0; i < order.length(); i++) {
             JSONObject innerObj = order.getJSONObject(i);
+            Map<String, String> person1 = new HashMap<>();
 
             String product_id = (String) innerObj.get("product_id");
             String product_name = (String) innerObj.get("product_name");
             int product_quantity = (int) innerObj.get("product_quantity");
-            String product_price = (String) innerObj.get("product_price");
+            double product_price = (double) innerObj.get("product_price");
+
+                person1.put("product_name", String.valueOf(product_name));//LOOP product_name
+                person1.put("product_id", String.valueOf(product_id));//LOOP product_id
+
+
+
+            System.out.println("product_id ==== " + product_id);
+
+
 
             if (
                     product_id.isEmpty()
                             || order_status.isEmpty()
-                            || total_price.isEmpty()
                             || order_id.isEmpty()
                             || product_name.isEmpty()
-                            || product_price.isEmpty()
+
                             || customer_id.isEmpty()
                             || customer_name.isEmpty()
                             || transaction_id.isEmpty()
@@ -115,8 +127,8 @@ public class CheckoutController {
 
 
             PreparedStatement statement = connection1.prepareStatement(sql1);
-            statement.setString(1, "4ce6c726-933b-42bc-8a91-a1765f754c4b");
-            statement.setDouble(2, 120);
+            statement.setString(1, product_id);
+            statement.setDouble(2, product_price);
 
             ResultSet resultSet = statement.executeQuery();
 
@@ -124,9 +136,14 @@ public class CheckoutController {
             while (resultSet.next()) {
                 // System.out.println("An existing user was updated successfully!");
                 String product_amount = resultSet.getString("product_amount");
+                String product_statusDB = resultSet.getString("product_status");
+
 
                 int amount = Integer.parseInt(product_amount);
                 System.out.println( "product_quantity ==== " + product_quantity);
+                System.out.println( "product_statusDB ==== " + product_statusDB);
+                person1.put("product_status", String.valueOf(product_statusDB));//LOOP product_status
+
                 System.out.println( "amount ==== " + amount);
                 if(amount >= product_quantity){
                     int new_amount = (amount - product_quantity);
@@ -135,24 +152,17 @@ public class CheckoutController {
                     if( new_amount > 0){
                         PreparedStatement statement1 = connection1.prepareStatement(updateProductAmount);
                         statement1.setInt(1, new_amount);
-                        statement1.setString(2, "4ce6c726-933b-42bc-8a91-a1765f754c4b");
+                        statement1.setString(2, product_id);
                         int rowsUpdated2 = statement1.executeUpdate();
 
-                        if (rowsUpdated2 > 0) {
-                            // System.out.println("An existing user was updated successfully!");
+                        // System.out.println("An existing user was updated successfully!");
 
-                            response.put("resultCode", "200");
-                            response.put("resultMsg", "OK, success");
-
-                            return ResponseEntity.ok(response);
-                        }
                     }else {
-
 
 
                         PreparedStatement statement10 = connection1.prepareStatement(updateProductAmount);
                         statement10.setInt(1, new_amount);
-                        statement10.setString(2, "4ce6c726-933b-42bc-8a91-a1765f754c4b");
+                        statement10.setString(2, product_id);
                         System.out.println( "new_amount ==== " + new_amount);
 
                         int rowsUpdated2 = statement10.executeUpdate();
@@ -161,29 +171,17 @@ public class CheckoutController {
                             int status_now = 0;
                             PreparedStatement statement3 = connection1.prepareStatement(updateProductStatus);
                             statement3.setInt(1, status_now);
-                            statement3.setString(2, "4ce6c726-933b-42bc-8a91-a1765f754c4b");
+                            statement3.setString(2, product_id);
                             int rowsUpdated3 = statement3.executeUpdate();
-                            if (rowsUpdated3 > 0) {
-                                // System.out.println("An existing user was updated successfully!");
+                            // System.out.println("An existing user was updated successfully!");
 
-                                response.put("resultCode", "200");
-                                response.put("resultMsg", "OK, success");
-
-                                return ResponseEntity.ok(response);
-                            }
                         }
                     }
 
 
-                }else {
-                    response.put("resultCode", "40633");
-                    response.put("resultMsg", "parameter not acceptable");//สินค้าหมด
-                    response.put("extraPara", "");
-
-                    return ResponseEntity.ok(response);
                 }
             }
-
+                resultMsg.add(person1);
         }
 
 
@@ -191,7 +189,7 @@ public class CheckoutController {
 
                 statement.setString(1, order_id);
                 statement.setString(2, customer_id);
-                statement.setString(3, total_price);
+                statement.setDouble(3, total_price);
                 statement.setString(4, datepro111);
                 statement.setString(5, order_status);
                 statement.setString(6, transaction_id);
@@ -209,6 +207,7 @@ public class CheckoutController {
 
                     response.put("resultCode", "200");
                     response.put("resultMsg", "OK, success");
+                    response.put("order_detail", resultMsg);
 
                     return ResponseEntity.ok(response);
                 }
