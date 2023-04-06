d171ee85e8e98b09eabd6860452775bde2819ec0
Max Santiphab <104512737+MaxSoudPouk@users.noreply.github.com>
2023-04-06 21:07:11 +0700

    +

---
+++ src/main/java/com/example/VintageShopAPI/showcustomercontect/DeleteCustomerContactController.java
@@ -0,0 +1,116 @@
+package com.example.VintageShopAPI.showcustomercontect;
+
+import com.example.VintageShopAPI.db.Config;
+import com.example.VintageShopAPI.db.DatabaseConnectionPool;
+import com.example.VintageShopAPI.security.JWT_Security_Encode_Decode_Java;
+import org.json.JSONException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+@RestController
+@CrossOrigin
+public class DeleteCustomerContactController {
+
+    @PostMapping("/v1/DeleteCustomerContact")
+    public ResponseEntity<Map<String, Object>> AddProduct(
+            @RequestParam("messageID") String messageID,
+            @RequestParam("token") String token,
+            @RequestParam("user_Email") String user_Email,
+            @RequestParam("user_ID") String user_ID
+    ) throws JSONException {
+
+        Map<String, Object> response = new HashMap<>();
+
+
+        if (messageID.equals("") || token.equals("") || user_Email.equals("") || user_ID.equals("")) {
+
+
+            response.put("resultCode", "406");
+            response.put("resultMsg", "parameter not acceptable");
+            response.put("extraPara", "");
+
+            return ResponseEntity.ok(response);
+
+        }
+//====================== Create date ======================//
+
+        Calendar currentDate111 = Calendar.getInstance();
+        SimpleDateFormat formatter111 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        formatter111.setTimeZone(TimeZone.getTimeZone("Asia/Bangkok"));
+        String dateNow111 = formatter111.format(currentDate111.getTime());
+        String datepro111 = dateNow111.toString();
+
+//====================== END Create date ======================//
+//=========================== DB ===========================//
+        PreparedStatement pstmt;
+        ResultSet rs;
+        DatabaseConnectionPool dbConnectionPool = null;
+        Connection connection1 = null;
+        String strRetunr = null;
+        Statement statementtAuth = null;
+        ResultSet resultSettAuth = null;
+        Connection conntAuth = null;
+        String sql = "UPDATE customer_message\n" +
+                "SET message_status_delete = '0', message_delete_date = '"+datepro111+"' \n" +
+                "WHERE message_id = '" + messageID + "';\n";
+
+
+//=========================== DB ===========================//
+        //validate JWT
+        boolean jwtencoderesult = false;
+
+        JWT_Security_Encode_Decode_Java encode_Decode_Java = new JWT_Security_Encode_Decode_Java();
+        jwtencoderesult = encode_Decode_Java.deCodeJWT_validate(token, user_Email, user_ID);
+
+        if (jwtencoderesult) {
+
+            try {
+                dbConnectionPool = new
+                        DatabaseConnectionPool(Config.driverServr,
+                        Config.dburlServr,
+                        Config.dbUserNameServr,
+                        Config.dbPasswordServr);
+                connection1 = dbConnectionPool.getConnection();
+                PreparedStatement statement = connection1.prepareStatement(sql);
+
+                int deletedRows = statement.executeUpdate();
+
+                if (deletedRows > 0) {
+                    response.put("resultCode", "200");
+                    response.put("resultMsg", "OK, success");
+                    response.put("Delete_Status", "OK");
+                    response.put("date", datepro111);
+
+                    return ResponseEntity.ok(response);
+                } else {
+                    response.put("resultCode", "216");
+                    response.put("resultMsg", "Error query Data Product");
+                    response.put("extraPara", "");
+
+                    return ResponseEntity.ok(response);
+                }
+
+            } catch (SQLException ex) {
+
+                response.put("resultCode", "216");
+                response.put("resultMsg", "Error query Data Product");
+                response.put("extraPara", "");
+
+                return ResponseEntity.ok(response);
+            }
+        } else {
+
+            response.put("resultCode", "498");
+            response.put("ResultMsg", "Invalid_Token");
+            return ResponseEntity.ok(response);
+
+        }
+    }
+}
